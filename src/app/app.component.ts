047d530dff02654ccf93ed7ac4a2fbafed320e8f
Travis Craft <tlcraft@users.noreply.github.com>
2020-02-29 12:01:26 -0700

    Update app component to call service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonApiService } from './services/pokemon-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pokemon-ng';
+  pokemon: any;
+
+  constructor(private pokemonApiService: PokemonApiService) {}
+
+  ngOnInit() {
+    this.pokemonApiService
+      .getPokemon()
+      .subscribe(pokemon => this.pokemon = pokemon); 
+  }
 }
