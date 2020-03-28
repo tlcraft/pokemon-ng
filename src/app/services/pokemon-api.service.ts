69121a6e615c4fc12b02ac829ae16691b2865cc2
Travis Craft <tlcraft@users.noreply.github.com>
2020-03-28 12:42:46 -0600

    Return Pokemon model from service.

--- src/app/services/pokemon-api.service.ts
+++ src/app/services/pokemon-api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { Pokemon } from '../models/pokemon.model';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,7 @@ export class PokemonApiService {
   constructor(private http: HttpClient) { }
 
   getPokemon(name: string): Observable<any> {
-    const pokemon = this.http.get<any>(environment.apiUrl + name);
+    const pokemon = this.http.get<Pokemon>(environment.apiUrl + name);
     return pokemon;
   }
 }
